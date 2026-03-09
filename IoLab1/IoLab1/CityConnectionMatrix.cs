bb31bbe7150189583870283dd09e154b6a1bc0e7
majewski maksymilian <majewskim@p30202.MINI>
2026-03-09 12:27:47 +0100

    data

--- IoLab1/IoLab1/CityConnectionMatrix.cs
+++ IoLab1/IoLab1/CityConnectionMatrix.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Dynamic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,19 @@ namespace IoLab1
 {
     internal class CityConnection
     {
+        public Dictionary<(String, String), int> _matrix;
+
+        public CityConnection()
+        {
+            _matrix = new Dictionary<(String, String), int>();
+            _matrix.Add(("A", "B"), 10);
+        }
+
+        public int Get((String,String) key)
+        {
+            _matrix.TryGetValue(key, out var value);
+            return value;
+        }
+       
     }
 }
