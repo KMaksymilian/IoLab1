14de2f8e2f083a7b4d841632ed138ac42889d460
majewski maksymilian <majewskim@p30202.MINI>
2026-03-09 12:21:52 +0100

    red tests

---
+++ IoLab1/IoLab1/CitySelector.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IoLab1
+{
+    public class CitySelector
+    {
+        public int Find(string v1, string v2)
+        {
+            return 1;
+        }
+    }
+}
